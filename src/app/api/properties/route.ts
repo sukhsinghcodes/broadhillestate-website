0266bf7e6800c642917357c3bbc1785138a8ab70
Sukh Singh <sukh@orbs.com>
2024-05-27 07:56:28 +0100

    integrated contentful client + types

---
+++ src/app/api/properties/route.ts
@@ -0,0 +1,68 @@
+import { TypeProperty } from '@/app/generated-types'
+import { client } from '../client'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const location = searchParams.get('location')
+  const transactionType = searchParams.get('transactionType')
+  const minPrice = searchParams.get('minPrice')
+  const maxPrice = searchParams.get('maxPrice')
+  const minBeds = searchParams.get('minBeds')
+  const availibility = searchParams.get('availibility')
+
+  const query = {} as Record<string, any>
+
+  if (location) {
+    // TODO: turn location into longitude and latitude
+  }
+
+  if (transactionType) {
+    query['fields.transactionType'] = transactionType
+  }
+
+  if (minPrice) {
+    query['fields.price[gte]'] = minPrice
+  }
+
+  if (maxPrice) {
+    query['fields.price[lte]'] = maxPrice
+  }
+
+  if (minBeds) {
+    query['fields.numberOfBedrooms[gte]'] = minBeds
+  }
+
+  if (availibility) {
+    query['fields.status'] = availibility
+  }
+
+  try {
+    const properties = await client.getEntries<TypeProperty>({
+      content_type: 'property',
+      select: [
+        'sys.id',
+        'fields.name',
+        'fields.description',
+        'fields.numberOfBedrooms',
+        'fields.numberOfBathrooms',
+        'fields.numberOfReceptions',
+        'fields.transactionType',
+        'fields.propertyType',
+        'fields.status',
+        'fields.price',
+        'fields.location',
+      ],
+      ...query,
+    })
+
+    console.log(
+      'properties',
+      properties.items.map((item) => item.sys.id)
+    )
+
+    return Response.json(properties)
+  } catch (err) {
+    console.error('error', err)
+    return Response.json({ error: err }, { status: 500 })
+  }
+}
---
+++ src/app/generated-types/TypeProperty.ts
@@ -0,0 +1,20 @@
+import type { Asset, Entry, EntryFields, EntrySkeletonType } from 'contentful'
+
+export interface TypePropertyFields {
+  name: EntryFields.Symbol
+  numberOfBedrooms: EntryFields.Integer
+  numberOfBathrooms: EntryFields.Integer
+  numberOfReceptions: EntryFields.Integer
+  price: EntryFields.Number
+  transactionType: 'Lettings' | 'Sales'
+  propertyType: 'Apartment' | 'Commercial' | 'House'
+  status: 'Available' | 'PendingCompletion' | 'Unavailable' | 'UnderOffer'
+  description: EntryFields.RichText
+  features: EntryFields.Symbol[]
+  location: EntryFields.Location
+  gallery?: Asset[]
+  floorplan?: Asset[]
+  epc?: Asset
+}
+
+export type TypeProperty = EntrySkeletonType<TypePropertyFields>
