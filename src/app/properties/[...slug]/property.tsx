b7a8511465ae464af89e935e9c4d3db2ed04b7cb
Sukh Singh <sukh@orbs.com>
2024-09-12 12:27:01 +0100

    fix features spacing

--- src/app/properties/[...slug]/property.tsx
+++ src/app/properties/[...slug]/property.tsx
@@ -168,9 +168,11 @@ export function Property({ id }: PropertyProps) {
           </div>
           <div className="mt-2">
             <H3 className="font-bold text-neutral-400">Property Features</H3>
-            <ul className="grid grid-rows-4 grid-flow-col mt-4">
+            <ul className="grid grid-rows-4 grid-flow-col m-0 mt-4">
               {data.features.map((feature) => (
-                <li key={feature}>{feature}</li>
+                <li key={feature} className="ml-6">
+                  {feature}
+                </li>
               ))}
             </ul>
           </div>
