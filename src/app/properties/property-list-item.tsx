93c2fcb017fa4a22287ca7b881d07345f6e0a1f3
Sukh Singh <sukh@orbs.com>
2024-11-14 14:42:52 +0000

    highlighted featured listings

--- src/app/properties/property-list-item.tsx
+++ src/app/properties/property-list-item.tsx
@@ -1,15 +1,16 @@
-import { H3 } from '../components/typography'
-import { Property, PropertyStatus, TransactionType } from '../types'
-import Image from 'next/image'
-import { getAvailabilityLabel, getSlug, pound } from '../utils'
-import { BathIcon, BedIcon, HomeIcon, SofaIcon } from 'lucide-react'
-import { Badge } from '@/components/ui/badge'
-import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
-import Link from 'next/link'
+import { H3 } from '../components/typography';
+import { Property, PropertyStatus, TransactionType } from '../types';
+import Image from 'next/image';
+import { getAvailabilityLabel, getSlug, pound } from '../utils';
+import { BathIcon, BedIcon, HomeIcon, SofaIcon, StarIcon } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export type PropertyListItemProps = {
-  property: Property
-}
+  property: Property;
+};
 
 export function PropertyListItem({
   property: {
@@ -24,15 +25,20 @@ export function PropertyListItem({
     numberOfBathrooms,
     numberOfBedrooms,
     numberOfReceptions,
+    featured,
   },
 }: PropertyListItemProps) {
-
-  const descPlainText = documentToPlainTextString(description)
-  const descText = descPlainText.length > 150 ? `${descPlainText.slice(0, 150)}...` : descPlainText
+  const descPlainText = documentToPlainTextString(description);
+  const descText = descPlainText.length > 150 ? `${descPlainText.slice(0, 150)}...` : descPlainText;
 
   return (
     <Link className="w-full" href={`/properties/${id}/${getSlug(name)}`}>
-      <div className="w-full flex flex-col md:flex-row bg-neutral-700 shadow-xl">
+      <div
+        className={cn(
+          'w-full flex flex-col md:flex-row bg-neutral-700 shadow-xl',
+          featured && 'border-b-2 border-primary'
+        )}
+      >
         <div className="md:basis-1/4">
           <Image
             src={`https:${gallery?.[0].fields.file?.url?.toString()}?fit=thumb&f=top_left&w=500&h=300`}
@@ -43,20 +49,20 @@ export function PropertyListItem({
           />
         </div>
         <div className="basis-3/4 p-4 md:p-8 flex flex-col gap-3 md:gap-6 justify-between">
+          {featured && (
+            <div>
+              <Badge className="uppercase">Featured</Badge>
+            </div>
+          )}
           <H3 className="m-0 font-normal">{name}</H3>
           <h4 className="text-3xl md:text-4xl">{`${pound.format(price)}${
             transactionType === TransactionType.Lettings ? ' PCM' : ''
           }`}</h4>
-          <div className="text-sm">
-            {descText}
-          </div>
+          <div className="text-sm">{descText}</div>
           <div className="flex justify-between gap-4 pt-3">
             <div className="flex flex-col md:flex-row gap-4 items-start md:items-center">
               <Badge variant="secondary" className="uppercase">
-                {getAvailabilityLabel(
-                  transactionType as TransactionType,
-                  status as PropertyStatus
-                )}
+                {getAvailabilityLabel(transactionType as TransactionType, status as PropertyStatus)}
               </Badge>
               <div className="flex gap-2 items-center">
                 <HomeIcon className="text-neutral-300 stroke-1" size={20} />
@@ -87,5 +93,5 @@ export function PropertyListItem({
         </div>
       </div>
     </Link>
-  )
+  );
 }
